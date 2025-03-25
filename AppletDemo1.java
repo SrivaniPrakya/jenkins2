db22811cb32d8c78e8371d52d6be4ac492fbb572
SrivaniPrakya <245122748053@mvsrec.edu.in>
2025-03-25 12:34:55 +0530

    Applet

---
+++ AppletDemo1.java
@@ -0,0 +1,14 @@
+import java.applet.*;
+import java.awt.*;
+/*<applet code="AppletDemo1.class" width="300" height="300">
+</applet>*/
+public class AppletDemo1 extends Applet
+{
+	public void paint(Graphics g)
+	{
+		g.drawString("Hello World!",100,150);
+		g.drawLine(150,300,250,300);
+		g.drawLine(175,350,175,550);
+		g.drawRect(200,400,100,150);
+	}
+}
