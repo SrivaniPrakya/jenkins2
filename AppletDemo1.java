839fdc0156f0882e05a66005cc9b32d06d616934
SrivaniPrakya <245122748053@mvsrec.edu.in>
2025-03-25 12:52:20 +0530

    Update AppletDemo1.java

--- AppletDemo1.java
+++ AppletDemo1.java
@@ -4,11 +4,15 @@ import java.awt.*;
 </applet>*/
 public class AppletDemo1 extends Applet
 {
-	public void paint(Graphics g)
-	{
-		g.drawString("Hello World!",100,150);
-		g.drawLine(150,300,250,300);
-		g.drawLine(175,350,175,550);
-		g.drawRect(200,400,100,150);
-	}
+        public void paint(Graphics g)
+        {
+                g.setColor(Color.green);
+                g.drawString("Hello World!",100,150);
+                g.drawLine(150,300,250,300);
+                g.drawLine(175,350,175,550);
+                g.setColor(Color.RED);
+                g.drawRect(200,400,100,150);
+                g.setColor(Color.blue);
+                g.fillOval(200,400,100,150);
+        }
 }
